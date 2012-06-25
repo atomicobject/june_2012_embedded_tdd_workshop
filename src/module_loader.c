9f1dd6ebc722a47d5a32d90f4c573f5324dd99f7
Matt Fletcher <fletcher@atomicobject.com>
2012-06-24 23:19:55 -0400

    implement data loader

--- src/module_loader.c
+++ src/module_loader.c
@@ -3,5 +3,9 @@
 
 Status_T ModuleLoader_LoadItem(unsigned int index)
 {
+  DataStruct_T * data = DataStore_RetrieveByIndex(index);
+  MemOpResult_T result = DataLoader_Load(data);
+
+  if (result == MEM_OK) return OK;
   return NOT_OK;
 }
